9c18d6f47d409d13c10f9d1ace284de8c471753c
kaleab16 <classkaleab@gmail.com>
2026-03-17 07:32:24 +0000

    0-isupper: implement _isupper function to check uppercase

--- 0-isupper.c
+++ 0-isupper.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - checks if a character is uppercase
- * @c: the character to check
+ * _isupper - checks for uppercase character
+ * @c: character to check
  *
- * Return: 1 if uppercase, 0 otherwise
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
     if (c >= 'A' && c <= 'Z')
         return (1);
-    return (0);
+    else
+        return (0);
 }
