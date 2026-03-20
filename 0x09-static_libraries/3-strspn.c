6a9b1aec86f854a4df9b2c709817c26cc4ec6100
kaleab16 <classkaleab@gmail.com>
2026-03-20 15:07:11 +0300

    Add static library project with libmy.a and create_static_lib.sh

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int i, found;
+
+	while (*s != '\0')
+	{
+		found = 0;
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (found)
+		{
+			count++;
+			s++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return (count);
+}
