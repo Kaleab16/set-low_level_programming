e1254b60bdb5d3053c8b6c5b373b082bc7c15530
kaleab16 <classkaleab@gmail.com>
2026-03-20 14:48:06 +0300

    Fix cap_string: declare j outside for loop for C89 compliance

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,6 +9,7 @@
 char *cap_string(char *str)
 {
 	int i = 0;
+	int j;
 	int capitalize_next = 1;
 	char separators[] = " \t\n,;.!?\"(){}";
 
@@ -20,7 +21,7 @@ char *cap_string(char *str)
 		}
 		capitalize_next = 0;
 
-		for (int j = 0; separators[j] != '\0'; j++)
+		for (j = 0; separators[j] != '\0'; j++)
 		{
 			if (str[i] == separators[j])
 			{
