e903c72eafb25c73eb03d32ea9dd0f534105edc8
Kaleab16 <classkaleab@gmail.com>
2026-06-22 02:27:10 -0700

    Add hash_table_get function

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -24,3 +24,5 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
 #endif
+
+char *hash_table_get(const hash_table_t *ht, const char *key);
