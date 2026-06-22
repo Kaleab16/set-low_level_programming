461487095ee39496742b61a763f558e732de1ae1
Kaleab16 <classkaleab@gmail.com>
2026-06-22 02:03:38 -0700

    Clean hash_table_create implementation

---
+++ hash_tables/README.md
@@ -0,0 +1,16 @@
+# Hash Tables
+
+This directory contains C programs implementing hash tables.
+
+## Files
+
+- `hash_tables.h` - Header file
+- `0-hash_table_create.c` - Creates a hash table
+
+## Compilation
+
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+
+## Author
+
+kaleab16
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -5,12 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * struct hash_node_s - Node of a hash table
- * @key: The key, string
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
- */
 typedef struct hash_node_s
 {
 char *key;
@@ -18,13 +12,6 @@ char *value;
 struct hash_node_s *next;
 } hash_node_t;
 
-/**
- * struct hash_table_s - Hash table data structure
- * @size: The size of the array
- * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list,
- * because we want our HashTable to use a Chaining collision handling
- */
 typedef struct hash_table_s
 {
 unsigned long int size;
